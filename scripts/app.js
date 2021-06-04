a0033f18261141a1a1e587fac935bb1a8d713a72
henarto2000 <71784943+henarto2000@users.noreply.github.com>
2021-06-04 19:40:55 +0700

    Create app.js

---
+++ scripts/app.js
@@ -0,0 +1,23 @@
+// memeriksa objek caches apakah tersedia pada objek global window , 
+// jika iya, request data dari caches
+if (!('caches' in window)) {
+  return null;
+}
+const url = `${window.location.origin}/forecast/${coords}`;
+return caches.match(url)
+    .then((response) => {
+      if (response) {
+        return response.json();
+      }
+      return null;
+    })
+    .catch((err) => {
+      console.error('Error getting data from cache', err);
+      return null;
+    });
+
+// CODELAB: Add code to call getForecastFromCache.
+getForecastFromCache(location.geo)
+    .then((forecast) => {
+      renderForecast(card, forecast);
+    });
